36251b4cf743fa698409758d768055e075c3e654
Nandan Thakur <nandant@gmail.com>
2022-06-01 22:54:05 +0200

    added util function in jpq for downloading

---
+++ income/jpq/util.py
@@ -0,0 +1,26 @@
+import requests
+from tqdm.autonotebook import tqdm
+import logging
+
+logger = logging.getLogger(__name__)
+
+def download_url(url: str, save_path: str, chunk_size: int = 1024):
+    """Download url with progress bar using tqdm
+    https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
+    Args:
+        url (str): downloadable url
+        save_path (str): local path to save the downloaded file
+        chunk_size (int, optional): chunking of files. Defaults to 1024.
+    """
+    r = requests.get(url, stream=True)
+    total = int(r.headers.get('Content-Length', 0))
+    with open(save_path, 'wb') as fd, tqdm(
+        desc=save_path,
+        total=total,
+        unit='iB',
+        unit_scale=True,    
+        unit_divisor=chunk_size,
+    ) as bar:
+        for data in r.iter_content(chunk_size=chunk_size):
+            size = fd.write(data)
+            bar.update(size)
